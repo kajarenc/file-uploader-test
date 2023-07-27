0f34203a8c9dd002457651b56d3fa78826fc9f45
Karen Javadyan <kajarenc@gmail.com>
2023-07-27 19:54:35 +0400

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+image = st.camera_input("CAMERA INPUT")
+
+if image:
+    st.image(image)
+
+single_file = st.file_uploader("SINGLE FILE UPLOADER")
+
+if single_file:
+    st.write(single_file.name)
+
+multiple_files = st.file_uploader("MULTIPLE FILE UPLOADER", accept_multiple_files=True)
+
+if multiple_files:
+    for f in multiple_files:
+        st.write(f.name)
